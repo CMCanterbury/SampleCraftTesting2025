15528582e74f7fd094194f9973c2b67a3ac08201
Syric <syric28@gmail.com>
2025-05-21 12:09:35 -0400

    Added a block

--- src/main/java/com/mrmacky/samplecrafttesting/registry/SCTBlocks.java
+++ src/main/java/com/mrmacky/samplecrafttesting/registry/SCTBlocks.java
@@ -2,6 +2,8 @@ package com.mrmacky.samplecrafttesting.registry;
 
 import com.mrmacky.samplecrafttesting.SampleCraftTesting;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -13,6 +15,9 @@ public class SCTBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(SampleCraftTesting.MOD_ID);
 
     //Blocks go here
+    public static final DeferredBlock<Block> EXAMPLE_BLOCK = registerWithItem("example_block",
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)));
+
 
     public static void register(IEventBus bus) {
         BLOCKS.register(bus);
